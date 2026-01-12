d1ecee33f432727664fec40e41c6a5f933c5f00b
channak087 <ckong@byupathway.edu>
2026-01-13 01:50:02 +0700

    new html css

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const today = new Date();
+const year = today.getFullYear();
+
+document.getElementById("currentyear").innerHTML = year;
+document.getElementById("lastModified").innerHTML = "Last Modification: " + document.lastModified;
